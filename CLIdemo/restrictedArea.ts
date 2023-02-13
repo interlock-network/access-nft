4fbd56d45eb9a8f3c33c087af06677b70ecc52d8
Blair <blairmunroakusa@wp.computer>
2023-02-13 08:29:16 -0900

    restrictedArea.ts: normalize whitespace

--- CLIdemo/restrictedArea.ts
+++ CLIdemo/restrictedArea.ts
@@ -54,19 +54,19 @@ io.on('connect', (socket) => {
 
     if (message == 'request-access') {
 
-			// deal with cleartext credentials
+      // deal with cleartext credentials
       const username = args[0];
       const password = args[1];
 
-			// get SHA256 hashes
+      // get SHA256 hashes
       const userhash = getHash(username);
       const passhash = getHash(password);
 
-			// free and cleanup sensitive info
-    	username = 0;
-		 	password = 0;
-			global.gc();
-		
+      // free and cleanup sensitive info
+      username = 0;
+       password = 0;
+      global.gc();
+    
       // fetch the passhash corresponding to userhash from blockchain
       const restrictedCredentialCheckChild = fork(restrictedCredentialCheck);
       restrictedCredentialCheckChild.send( {userhash: userhash, passhash: passhash} );
@@ -93,20 +93,20 @@ io.on('connect', (socket) => {
             `login success for client on socket ` + cyan(`ID ${socket.id}`));
           socket.emit('access-granted');
 
-					// any further messages are client requests to restricted area server
+          // any further messages are client requests to restricted area server
           socket.onAny((session) => {
 
             // RESTRICTED AREA BELOW!!!
             //
             // only the privileged few who possess a
             // universal access NFT may interact in this space
-						//
-						// This space (its functionality) will vary according to the given
-						// universal access nft application
+            //
+            // This space (its functionality) will vary according to the given
+            // universal access nft application
             //
             var somethingUseful = false;
 
-							// a cheeky functionality
+              // a cheeky functionality
               if (session == 'do-something-useful') {
 
                 somethingUseful = true;
@@ -115,7 +115,7 @@ io.on('connect', (socket) => {
                   `just did something extremely useful`);
                 socket.emit('did-something-useful', somethingUseful);
 
-							// another cheeky functionality
+              // another cheeky functionality
               } else if (session == 'do-something-useless') {
 
                 somethingUseful = false;
@@ -124,14 +124,14 @@ io.on('connect', (socket) => {
                   `just did something extremely useless`);
                 socket.emit('did-something-useless', somethingUseful);
 
-							// serve the client some cool graphics
+              // serve the client some cool graphics
               } else if (session == 'fetch-art') {
 
                 console.log(magenta(`ACCESSNFT: `) +
                   `${username} on socket ` + cyan(`ID ${socket.id}`) +
                   `just fetched ascii art`);
 
-								// generate ascii art
+                // generate ascii art
                 figlet('(:  RESTRICTED AREA  :)\n\n\n\n\n\n         ...YOU ROCK!!!', function(err, data) {
                   
                   if (err) {
@@ -149,7 +149,7 @@ io.on('connect', (socket) => {
                   `just logged out`);
                 socket.disconnect();
               }
-						//
+            //
             //
             // RESTRICTED ACCESS AREA ABOVE!!!
           });
